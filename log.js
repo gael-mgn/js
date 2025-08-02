34539835e99522e362e891f20e6540a7059e42a7
gael-mgn <gael.maignan@eivp-paris.fr>
2025-08-02 11:53:28 +0200

    Add files via upload

--- log.js
+++ log.js
@@ -11,7 +11,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const path = window.location.pathname;       // Chemin après le domaine (ex: /page1/test)
     const isMobile = /Mobi|Android/i.test(navigator.userAgent) ? "mobile" : "desktop";
     const referrer = document.referrer;
-    googleSheets([domain, path, referrer, isMobile], "11cwEfj8x5KkiX9z5081IN25xVCUD49bIaP0ltizI6l8", domain, true);
+
+    // Récupérer les paramètres de l'URL après le "?"
+    const urlParams = new URLSearchParams(window.location.search);
+    let formattedParams = "";
+    urlParams.forEach((value, key) => {
+      formattedParams += `${key} : ${value}\n`;
+    });
+    // Supprimer le dernier saut de ligne si la chaîne n'est pas vide
+    if (formattedParams.length > 0) {
+      formattedParams = formattedParams.slice(0, -1);
+    }
+
+    googleSheets([domain, path, referrer, isMobile, formattedParams], "11cwEfj8x5KkiX9z5081IN25xVCUD49bIaP0ltizI6l8", domain, true);
     }
 });
 
